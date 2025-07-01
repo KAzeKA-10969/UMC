7328a0101ee18e59ca1434ce3c13b87094f2712d
KAzeKA-10969 <1239081885@qq.com>
2025-07-01 11:43:16 +0800

    Add files via upload

---
+++ configs/__init__.py
@@ -0,0 +1,4 @@
+pretrained_models_path = {
+    'bert': 'C:/Machine_Learning/UMC/uncased_L-12_H-768_A-12/bert_config.json',
+    'bert-base-uncased': 'C:/Machine_Learning/UMC/uncased_L-12_H-768_A-12/bert_config.json',
+}
